0480823263bc0140061daab3e0e6d4b713d466a8
Marlon Paulo <marlon.paulo.silva@outlook.com>
2022-08-09 09:25:16 -0300

    adicionando eventos com Ajax via JSON

--- controllers/ControllerSelectable.php
+++ controllers/ControllerSelectable.php
@@ -5,5 +5,14 @@
   $objEvents = new \Classes\ClassEvents();
 
   $json = json_decode(file_get_contents('php://input'));
-
   var_dump($json);   
+  
+  $start = new \DateTime($json->start);
+  $end = new \DateTime($json->end);
+  
+  var_dump($start);   
+  var_dump($end);   
+
+  $objEvents->createEvent(0, $json->title, '', 'red', $start->format("Y-m-d H:i:s"), $end->modify("-1 day")->format("Y-m-d H:i:s"));
+
+
